8caad66d31337b8abd60721996ed3c4649a06686
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2023-11-15 14:16:42 +0300

    betty

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <time.h>
+
+extern char **environ;
+
+int d_prompt(const char *prompt, unsigned int size);
+#endif
